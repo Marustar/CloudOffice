f3eb529b9c1c0b590132c8eba0ae47098bc1cad0
bjpark21 <bzpark@naver.com>
2023-06-14 23:18:09 +0900

    파일 송,수신자 변수 출력 + 오류 수정

--- Document/views.py
+++ Document/views.py
@@ -8,41 +8,45 @@ from docx import Document
 from django.conf import settings
 from docx2pdf import convert
 import os
+from Emp import models as Emp
+import pythoncom
 
+def findUser(request):
+    return Emp.Employee.objects.get(Emp_User = request.user)
+    
 @login_required
 def document_upload(request):
-    document_types = [1, 2, 3]    
+    pythoncom.CoInitialize()
+    document_types = [1, 2, 3]
     user = request.user
+    form = DocumentForm()  
     try:
         employee = Employee.objects.get(Emp_User=user)
     except Employee.DoesNotExist:
-        return redirect('login')
+        return render(request, 'fileupload.html', {'form': form})  
+
+    currentUser = findUser(request)
+
+    rank_dict = {1: "사원", 2: "대리", 3: "과장", 4: "차장", 5: "부장", 6: "사장"}
+    current_rank = rank_dict.get(currentUser.Emp_Rank, "")
 
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+
         if form.is_valid():
             form.instance.Doc_Dept = employee.Emp_Dept
-            
             document = request.FILES.get('document')
             if document:
                 document_name = process_file(document)
                 document_path = os.path.join(settings.BASE_DIR, 'DocumentData', document_name)
-                
                 file_name = os.path.splitext(document_name)[0]
                 file_extend = os.path.splitext(document_name)[1]
-                
                 file_ist = File()
                 file_ist.File_Name = file_name + file_extend
                 file_ist.File_Extend = file_extend
                 file_ist.save()
                 form.instance.Doc_Files = file_ist
-                
-                response_data = {
-                    'status': 'success',
-                    'document_name': document.name,
-                }
-                
-            
+
             form.save()
             return redirect('/index.html')
         else:
@@ -50,8 +54,17 @@ def document_upload(request):
     else:
         form = DocumentForm()
 
-    return render(request, 'fileupload.html', {'form': form, 'employee': employee, 'document_types': document_types})
+    employees = Employee.objects.all()  
+    context = {
+        'form': form,
+        'username': employee.Emp_Name, 
+        'Rank': current_rank,  
+        'employee': employee,
+        'employees': employees,  
+        'document_types': document_types
+    }
 
+    return render(request, 'fileupload.html', context)
 
 def convert_ppt_to_pdf(file):
     file_extension = os.path.splitext(file.name)[1].lower()
