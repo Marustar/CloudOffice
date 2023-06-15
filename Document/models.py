e7908a60f9cf93f05a3e74062137b390b6c0ddc2
bjpark21 <bzpark@naver.com>
2023-06-15 22:54:09 +0900

    보고일자 한국 시간대 추가

--- Document/models.py
+++ Document/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from Emp.models import Employee, Department
-
-# Create your models here.
-
+from django.utils import timezone
 
 class File(models.Model):
     File_Name = models.CharField(max_length=100)
@@ -25,6 +23,14 @@ class Document(models.Model):
     Doc_State = models.IntegerField()
     Doc_Content = models.TextField(default="")
     Doc_Check = models.IntegerField(null=True)
+    Doc_Time = models.DateTimeField(default=timezone.now)
+
+    def Doc_Time2(self):
+        dt = self.Doc_Time
+        if dt.hour < 12:
+            return dt.strftime('%Y년 %m월 %d일 오전 %I:%M:%S')
+        else:
+            return dt.strftime('%Y년 %m월 %d일 오후 %I:%M:%S')
 
     def __str__(self):
         return self.Doc_Title
